4dafb8b2e2b93023d80bd67bb47798ebeef199e6
katznicho <katznicho@gmail.com>
2026-05-13 21:22:32 +0300

    end-no-edit

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -14,6 +14,7 @@ class DepartmentController extends Controller
             return redirect()->route('dashboard')
                 ->with('error', 'You must be associated with an insurance company to manage departments.');
         }
+        
 
         return null;
     }
